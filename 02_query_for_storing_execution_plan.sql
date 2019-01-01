1ce42e513ddb6c39bffa8f9030194d4e72ed81e2
tomaz <tomaz.kastrun@outlook.com>
2019-01-01 12:05:42 +0100

    adding files

---
+++ 02_query_for_storing_execution_plan.sql
@@ -0,0 +1,14 @@
+USE AdventureWorks;
+GO
+
+/*
+
+ - Storing Execution plan in table
+ - running R dataframes and stats using sp_execute_External_Script
+ 
+Author: Tomaz Kastrun
+Date: 1.1.2019
+Blog: tomaztsql.wordpress.com
+
+
+*/
